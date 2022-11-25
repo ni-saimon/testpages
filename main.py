866dc19b3258b2d706c61ea858f880eb4f5a9177
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-11-25 20:55:58 +0600

    test setup

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
-class Automation():
 
-    def setUp(self):
+class Automation:
+
+    def __init__(self):
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.driver.maximize_window()
+
+    def setup(self):
         self.driver.get("https://testpages.herokuapp.com/")
+        assert self.driver.find_element(By.XPATH, "/html/body/div/h1").text == "Test Pages For Automating"
+
+    def shutdown(self):
+        self.driver.close()
 
 
 o = Automation()
-o.setUp()
+o.setup()
+# o.shutdown()
